cf7f21185e81384383134696a17dafe9c64ad8dc
Mukul Aryal <aryalmukul@gmail.com>
2023-11-26 20:14:34 +0545

    Fixed same minor bug again

--- cdn/views.py
+++ cdn/views.py
@@ -79,10 +79,13 @@ class DocumentView(APIView):
         try:
             if type(request.user) == AnonymousUser:
                 return Response({"error": "Unauthorized"}, status=401)
-            document_id = request.query_params["document"]
-            if document_id:
-                document = Document.objects.get(pk=document_id)
-                return Response({"body":document.body}, status=200)
+            try:
+                document_id = request.query_params["document"]
+                if document_id:
+                    document = Document.objects.get(pk=document_id)
+                    return Response({"body":document.body}, status=200)
+            except:
+                pass
             project_id = request.query_params["project"]
             project = Project.objects.get(pk=project_id)
             user_in_project = UsersInProjects.objects.filter(
